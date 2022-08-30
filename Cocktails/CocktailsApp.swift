e5b2ff69ff6add7e65aced789c1eae8cee8d002d
repfly <yldrma9@gmail.com>
2022-08-30 14:02:05 +0300

    Initial Commit

---
+++ Cocktails/CocktailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsApp.swift
+//  Cocktails
+//
+//  Created by Alper Yıldırım on 30.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CocktailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
